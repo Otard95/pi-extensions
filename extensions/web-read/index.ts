8cd09f390c9f32fa284286afdb9bacc3ba5a63f0
Stian M <otard.code@proton.me>
2026-04-12 14:02:20 +0200

    chore(web-read): Fix import ordering

--- extensions/web-read/index.ts
+++ extensions/web-read/index.ts
@@ -1,4 +1,4 @@
-import { keyHint, type ExtensionAPI } from "@mariozechner/pi-coding-agent";
+import { type ExtensionAPI, keyHint } from "@mariozechner/pi-coding-agent";
 import { Text } from "@mariozechner/pi-tui";
 import { type Static, Type } from "@sinclair/typebox";
 import { fetchPage } from "./fetch";
