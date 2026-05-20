dbf0096487ca185985268cb9fa87a56598ddc5c5
Stian M <otard.code@proton.me>
2026-05-20 10:40:00 +0200

    fix(once): Correct return type to reflect undefined on subsequent calls

--- utils/func/once.ts
+++ utils/func/once.ts
@@ -4,11 +4,11 @@
  */
 export function once<A extends unknown[], R>(
 	fn: (...args: A) => R,
-): (...args: A) => R {
+): (...args: A) => R | undefined {
 	let hasRun = false;
 	let result: R | undefined;
 	return (...args: A) => {
-		if (hasRun) return result as R;
+		if (hasRun) return result;
 		hasRun = true;
 		result = fn(...args);
 		return result;
