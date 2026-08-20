e35cab84f30561fe1f462d88eabf573e72a0f3dd
Stian M <otard.code@proton.me>
2026-08-20 14:47:06 +0200

    chore: Bump version to 0.17.0

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
         packages = {
           default = pkgs.buildNpmPackage {
             pname = "pi-extensions";
-            version = "0.16.0";
+            version = "0.17.0";
 
             src = self;
 
